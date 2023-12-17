5db14bb753f74131ebb2d2b5f4fd1bc7e715e377
taKana671 <kanae5321@gmail.com>
2023-12-18 00:23:11 +0900

    First commit

---
+++ natures.py
@@ -0,0 +1,50 @@
+from panda3d.core import NodePath
+from panda3d.core import Vec3, BitMask32, TransformState
+from panda3d.bullet import BulletRigidBodyNode
+from panda3d.bullet import BulletCylinderShape, ZUp
+from panda3d.bullet import BulletBoxShape, BulletConvexHullShape
+
+from geomnode_maker import Sphere
+
+
+MASK = BitMask32(1) | BitMask32.bit(2)
+
+
+class Tree(NodePath):
+
+    def __init__(self, pos, angle, parent):
+        super().__init__(BulletRigidBodyNode('tree'))
+        model = base.loader.loadModel('models/plant6/plants6')
+        model.set_scale(0.5)
+        model.set_transform(TransformState.make_pos(Vec3(0, 0, -10)))
+        model.reparent_to(self)
+
+        end, tip = model.get_tight_bounds()
+        height = (tip - end).z
+        shape = BulletCylinderShape(1, height, ZUp)
+        self.node().add_shape(shape)
+
+        self.set_collide_mask(MASK)
+        self.set_h(angle)
+        self.set_pos(pos)
+        self.reparent_to(parent)
+
+
+class Rock(NodePath):
+
+    def __init__(self, pos, hpr, parent):
+        super().__init__(BulletRigidBodyNode('rock'))
+        model = Sphere(segments=8)
+        tex = base.loader.load_texture('textures/rock1.jpg')
+        model.set_texture(tex)
+        model.reparent_to(self)
+
+        shape = BulletConvexHullShape()
+        shape.add_geom(model.node().get_geom(0))
+
+        self.node().add_shape(shape)
+        self.set_collide_mask(MASK)
+        self.set_hpr(hpr)
+        self.set_pos(pos)
+        self.set_scale(Vec3(5, 3, 5))
+        self.reparent_to(parent)
